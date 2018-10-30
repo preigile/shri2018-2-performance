999f0200af74d0c7dea6104dc6a280c5184a33fc
preigile <diva.artim@gmail.com>
2018-10-30 18:13:24 +0300

    create app folder

---
+++ app/app.js
@@ -0,0 +1,5 @@
+import App from "./components/App";
+
+let root = document.getElementById('root');
+
+root.innerHTML = App();
---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+// import Header from "./Header";
+import Main from "./Main";
+// import Scenarios from "./Scenarios";
+// import Devices from "./Devices";
+// import Footer from "./Footer";
+
+export default () => {
+    return `${Main()}`;
+}
