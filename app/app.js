0bb3f356f0e9d206b937b1f644c728cae747b04e
preigile <diva.artim@gmail.com>
2018-10-30 19:45:03 +0300

    move dependencies to app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 import App from "./components/App";
 
+// import './scripts/scripts';
+import './styles/bootstrap.css';
+import './styles/styles.css';
+
 const root = document.getElementById('root');
 
 console.log('app', App());
