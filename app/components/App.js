d6c55d99244c8e511c60800fbdd2aa66b3c0bed1
preigile <diva.artim@gmail.com>
2018-10-31 00:36:53 +0300

    get rid of parcel

--- app/components/App.js
+++
@@ -1,9 +0,0 @@
-// import Header from "./Header";
-import Main from "./Main";
-// import Scenarios from "./Scenarios";
-// import Devices from "./Devices";
-// import Footer from "./Footer";
-
-export default () => {
-    return `${Main()}`;
-}
